920a2d95ff4eb2bea5b3971de209a419490614f0
Nias26 <genangeliludovico26@gmail.com>
2023-11-05 13:41:55 +0100

    Colorscheme

--- lua/keybinds.lua
+++ lua/keybinds.lua
@@ -8,3 +8,44 @@ vim.keymap.set('n', '<Leader>e', '<cmd>Ex<CR>')
 vim.keymap.set('n', '<', '<cmd>lua vim.diagnostic.open_float{focusable = false}<CR>')
 vim.keymap.set('n', 'bd', '<cmd>bdel<CR>')
 vim.keymap.set('n', 'ba', function() vim.ui.input({prompt = "New Buffer"}, function(input) vim.cmd({ cmd = 'badd', args = {input} }) end)end)
+vim.keymap.set('n', '<Leader>p', function() vim.ui.select({
+	'buf_lines',
+	'buffers',
+	'cli',
+	'commands',
+	'diagnostic',
+	'explorer',
+	'files',
+	'git_branches',
+	'git_commits',
+	'git_files',
+	'hit_hunks',
+	'grep',
+	'grep_live',
+	'help',
+	'hipatterns',
+	'history',
+	'hl_groups',
+	'keymaps',
+	'list',
+	'lsp',
+	'makrs',
+	'oldfiles',
+	'options',
+	'registers',
+	'resume',
+	'spellsuggest',
+	'treesitter'
+},	{ prompt = "Pick " },
+function(choice)
+	return vim.cmd({ cmd = 'Pick', args = {choice}})
+end)end)
+
+-- FIXME:DOesn't work for now.. (see mini-modules/mini.base16)
+-- vim.keymap.set('n', '<Leader>c', function() vim.ui.select({
+-- 	'oxocarbon',
+-- 	'kanagawa'
+-- }, { prompt = "Change base16 Colorscheme" },
+-- function(choice)
+-- 	return colorscheme == choice
+-- end)end)
--- lua/mini-modules/mini-base16.lua
+++ lua/mini-modules/mini-base16.lua
@@ -1,20 +1,64 @@
+
+--FIXME: Doesn't work for now...
+-- if colorscheme == "oxocarbon" then
+-- 	local colors = {
+-- 		-- Oxocarbon
+-- 		base00 = "#161616",
+-- 		base01 = "#262626",
+-- 		base02 = "#393939",
+-- 		base03 = "#525252",
+-- 		base04 = "#dde1e6",
+-- 		base05 = "#f2f4f8",
+-- 		base06 = "#ffffff",
+-- 		base07 = "#08bdba",
+-- 		base08 = "#3ddbd9",
+-- 		base09 = "#78a9ff",
+-- 		base0A = "#ee5396",
+-- 		base0B = "#33b1ff",
+-- 		base0C = "#ff7eb6",
+-- 		base0D = "#42be65",
+-- 		base0E = "#be95ff",
+-- 		base0F = "#82cfff",
+-- 	}
+-- elseif colorscheme == "kanagawa" then
+-- 	local colors = {
+-- 		-- Kanagawa
+-- 		base00 = "#1F1F28",
+-- 		base01 = "#2A2A37",
+-- 		base02 = "#223249",
+-- 		base03 = "#727169",
+-- 		base04 = "#C8C093",
+-- 		base05 = "#DCD7BA",
+-- 		base06 = "#938AA9",
+-- 		base07 = "#363646",
+-- 		base08 = "#C34043",
+-- 		base09 = "#FFA066",
+-- 		base0A = "#DCA561",
+-- 		base0B = "#98BB6C",
+-- 		base0C = "#7FB4CA",
+-- 		base0D = "#7E9CD8",
+-- 		base0E = "#957FB8",
+-- 		base0F = "#D27E99",
+-- 	}
+-- end
 require("mini.base16").setup({
 	palette = {
-		base00 = "#161616",
-		base01 = "#262626",
-		base02 = "#393939",
-		base03 = "#525252",
-		base04 = "#dde1e6",
-		base05 = "#f2f4f8",
-		base06 = "#ffffff",
-		base07 = "#08bdba",
-		base08 = "#3ddbd9",
-		base09 = "#78a9ff",
-		base0A = "#ee5396",
-		base0B = "#33b1ff",
-		base0C = "#ff7eb6",
-		base0D = "#42be65",
-		base0E = "#be95ff",
-		base0F = "#82cfff",
+		base00 = "#1F1F28",
+		base01 = "#2A2A37",
+		base02 = "#223249",
+		base03 = "#727169",
+		base04 = "#C8C093",
+		base05 = "#DCD7BA",
+		base06 = "#938AA9",
+		base07 = "#363646",
+		base08 = "#C34043",
+		base09 = "#FFA066",
+		base0A = "#DCA561",
+		base0B = "#98BB6C",
+		base0C = "#7FB4CA",
+		base0D = "#7E9CD8",
+		base0E = "#957FB8",
+		base0F = "#D27E99",
+
 	}
 })
