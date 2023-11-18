0875489118823998b0f70072a6c08f2046f2dcd4
Nias26 <genangeliludovico26@gmail.com>
2023-11-18 17:04:53 +0100

    FIX: Addeed descriptions for Mini.clue

--- lua/keybinds.lua
+++ lua/keybinds.lua
@@ -1,10 +1,10 @@
 vim.keymap.set('n', '<Tab>', '<cmd>bnext<CR>')
 vim.keymap.set('n', '<S-Tab>', '<cmd>bprev<CR>')
-vim.keymap.set('n', '<Leader>f', '<cmd>lua MiniFiles.open()<CR>')
-vim.keymap.set('n', '<Leader>t', '<cmd>Telescope<CR>')
+vim.keymap.set('n', '<Leader>f', '<cmd>lua MiniFiles.open()<CR>', {desc = "[Mini.files] Find Files"})
+vim.keymap.set('n', '<Leader>t', '<cmd>Telescope<CR>', {desc = "[Telescope] Open Telescope"})
 vim.keymap.set('n', '<F5>', '<cmd>lua MiniMap.toggle()<CR>')
-vim.keymap.set('n', '<Leader>z', '<cmd>lua MiniMisc.zoom()<CR>')
-vim.keymap.set('n', '<Leader>e', '<cmd>Ex<CR>')
+vim.keymap.set('n', '<Leader>z', '<cmd>lua MiniMisc.zoom()<CR>', {desc = "[Mini.misc] Zoom"})
+vim.keymap.set('n', '<Leader>e', '<cmd>Ex<CR>', {desc = "[netwr] Explore files"})
 vim.keymap.set('n', '<', '<cmd>lua vim.diagnostic.open_float{focusable = false}<CR>')
 vim.keymap.set('n', 'bd', '<cmd>bdel<CR>')
 vim.keymap.set('n', 'ba', function() vim.ui.input({prompt = "New Buffer"}, function(input) vim.cmd({ cmd = 'badd', args = {input} }) end)end)
@@ -39,7 +39,7 @@ vim.keymap.set('n', '<Leader>p', function() vim.ui.select({
 },	{ prompt = "Pick " },
 function(choice)
 	return vim.cmd({ cmd = 'Pick', args = {choice}})
-end)end)
+end)end, {desc = "[Mini.pick] Pick ..."})
 
 -- FIXME:DOesn't work for now.. (see mini-modules/mini.base16)
 -- vim.keymap.set('n', '<Leader>c', function() vim.ui.select({
