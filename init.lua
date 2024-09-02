5e19db4ed107205bd013447a9a0b0c1fcc9ae599
Nias26 <genangeliludovico26@gmail.com>
2024-09-02 23:16:14 +0200

    refactor(plugin-manager)!: moved from lazy.nvim to mini.deps

--- init.lua
+++ init.lua
@@ -1,33 +1,33 @@
 -- Require Options
 require("opts")
 
--- Bootstral lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-   "git",
-   "clone",
-   "--filter=blob:none",
-   "https://github.com/folke/lazy.nvim.git",
-   "--branch=stable", -- latest stable release
-   lazypath,
-   })
 end
-vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-	{import = "mini"},
-	{import = "plugins"}
 }
 
-local opts = {}
-
-require("lazy").setup(plugins, opts)
 
 -- Source every lua file in mini-modules
 for _, file in ipairs(vim.fn.readdir(vim.fn.stdpath('config')..'/lua/mini-modules', [[v:val =~ '\.lua$']])) do
   require('mini-modules.'..file:gsub('%.lua$', ''))
+-- Bootstrap mini.nvim
+local path_package = vim.fn.stdpath("data") .. "/site"
+local mini_path = path_package .. "/pack/deps/start/mini.nvim"
+if not vim.loop.fs_stat(mini_path) then
+  vim.cmd('echo "Installing `mini.nvim`" | redraw')
+  local clone_cmd = {
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "--branch",
+    "stable",
+    "https://github.com/echasnovski/mini.nvim",
+    mini_path,
+  }
+  vim.fn.system(clone_cmd)
+  vim.cmd("packadd mini.nvim | helptags ALL")
 end
 
 -- Require keybinds
+-- Setup mini.deps
+require("mini.deps").setup({ path = { package = path_package } })
 require("keybinds")
